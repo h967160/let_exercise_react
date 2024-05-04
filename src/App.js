649c6275ddb9696f9c2a823fb4e2f34753783052
Huang <h96716@gmail.com>
2024-05-04 20:38:02 +0800

    feat: add Footer to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Footer from "./components/Layouts/Footer/Footer";
-import Header from "./components/Layouts/Header/Header";
-import Main from "./components/Main/Main";
+import Header from "@/components/Layouts/Header/Header";
+import Footer from "@/components/Layouts/Footer/Footer";
+import Main from "@/components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
