6111cbd5292f960271a6c7c67c4853e47eb6e310
Huang <h96716@gmail.com>
2024-05-07 23:56:58 +0800

    refactor: move Header and Footer components into HomePage, App.js just for routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import Header from "@/components/Layouts/Header/Header";
-import Footer from "@/components/Layouts/Footer/Footer";
 import { HomePage } from "@/pages/index";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
         <Routes>
           {/* <Route path="login" element={<LoginPage />} /> */}
           {/* 
-          <Route path="signup" element={<SignUpPage />} />
-          <Route path="todo" element={<TodoPage />} /> */}
+          <Route path="signup" element={<SignUpPage />} /> */}
           <Route path="*" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,17 @@
 import Background from "@/components/Main/Background/Background";
 import Main from "@/components/Main/Main";
+import Footer from "@/components/Layouts/Footer/Footer";
+import Header from "@/components/Layouts/Header/Header";
 
 const HomePage = () => {
   return (
-    <Main>
-      <Background />
-    </Main>
+    <>
+      <Header />
+      <Main>
+        <Background />
+      </Main>
+      <Footer />
+    </>
   );
 };
 
