03780d995f40fc5a37e3c45652044708f58ddb2c
Huang <h96716@gmail.com>
2024-05-09 18:58:47 +0800

    fix: add node-polyfill-webpack-plugin and configure polyfill using craco to resolve json web token issue

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
   webpack: {
     // 建立路徑別名
@@ -6,5 +8,8 @@ module.exports = {
       // 將 "@" 設置為別名, 指向根目錄下src作為絕對路徑
       "@": path.resolve(__dirname, "src"),
     },
+    plugins: [
+      new NodePolyfillPlugin(),
+    ],
   },
 };
