40749238b518f42487f9c83f053581b4a207e667
Huang <h96716@gmail.com>
2024-06-28 23:58:39 +0800

    feat: add TabContent component

---
+++ src/components/User/Tabs/TabContent.jsx
@@ -0,0 +1,11 @@
+import styles from "./Tabs.module.scss";
+
+const TabContext = () => {
+  return (
+    <section className={styles.tabContent}>
+      <h1>我是tab區塊</h1>
+    </section>
+  );
+};
+
+export default TabContext;
--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Layouts/Footer/Footer";
 import Header from "@/components/Layouts/Header/Header";
 import UserProfileInfo from "@/components/User/UserProfile/UserProfileInfo";
 import Tabs from "@/components/User/Tabs/Tabs";
+import TabContent from "@/components/User/Tabs/TabContent";
 
 const UserProfilePage = () => {
   return (
@@ -12,6 +13,7 @@ const UserProfilePage = () => {
         <div className="container">
           <UserProfileInfo />
           <Tabs />
+          <TabContent />
         </div>
       </Main>
       <Footer />
